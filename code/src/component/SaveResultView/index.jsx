598cc945e17a7b1da654c9b1323a666fd261d676
LAPTOP-RI7D261L\Mr Ke <545800322@qq.com>
2020-01-08 14:40:59 +0800

    2

--- code/src/component/SaveResultView/index.jsx
+++ code/src/component/SaveResultView/index.jsx
@@ -63,11 +63,13 @@ class TreeView extends React.Component {
       fieldList = fieldList.map(({ fieldType1, fieldType2, ...a }, idx) => {
         if (idx == index) {
           return {
+            ...a,
             fieldType2: true,
             fieldType1: false
           }
         } else {
           return {
+            ...a,
             fieldType2: false,
             fieldType1
           }
